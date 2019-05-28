10df31ac2a120e10be386d53bba07755f35c84fe
Shining Xu <xushangning@sjtu.edu.cn>
2019-05-28 16:27:51 +0800

    Complete the ALU and the CU

--- single_cycle_computer/alu.v
+++ single_cycle_computer/alu.v
@@ -1,25 +1,21 @@
 module alu (a,b,aluc,s,z);
    input [31:0] a,b;
    input [3:0] aluc;
-   output [31:0] s;
-   output        z;
-   reg [31:0] s;
-   reg        z;
-   always @ (a or b or aluc) 
-      begin                                   // event
-         casex (aluc)
-             4'bx000: s = a + b;              //x000 ADD
-                                              //x100 SUB
-             // complete by yourself          //x001 AND
-                                              //x101 OR
-                                              //x010 XOR
-                                              //x110 LUI: imm << 16bit             
-                                              //0011 SLL: rd <- (rt << sa)
-                                              //0111 SRL: rd <- (rt >> sa) (logical)
-             4'b1111: s = $signed(b) >>> a;   //1111 SRA: rd <- (rt >> sa) (arithmetic)
-             default: s = 0;
-         endcase
-         if (s == 0 )  z = 1;
-            else z = 0;         
-      end      
+   output reg [31:0] s;
+   output reg z;
+   always @ (a or b or aluc) begin
+      casex (aluc)
+         4'bx000: s = a + b;
+         4'bx100: s = a - b;
+         4'bx001: s = a & b;
+         4'bx101: s = a | b;
+         4'bx010: s = a ^ b;
+         4'bx110: s = {b[15:0], a[15:0]};    // lui
+         4'b0011: s = b << a;
+         4'b0111: s = b >> a;
+         4'b1111: s = $signed(b) >>> a;      // sra
+         default: s = 0;
+      endcase
+      z = (s == 0);
+   end
 endmodule
--- single_cycle_computer/sc_cu.v
+++ single_cycle_computer/sc_cu.v
@@ -1,7 +1,11 @@
 /**
+* wmem: Enable memory write if true
+* regrt: True if rt is the register to write
+* m2reg: Data read from the data memory will be written to the register if true.
 * aluc: Control signals for the ALU
 * shift: The first operand of ALU comes from shamt (true) or rs
-* sext: sign extension
+* jal: True if $ra will be written
+* sext: sign extension if true
 */
 module sc_cu (op, func, z, wmem, wreg, regrt, m2reg, aluc, shift,
               aluimm, pcsource, jal, sext);
@@ -15,50 +19,46 @@ module sc_cu (op, func, z, wmem, wreg, regrt, m2reg, aluc, shift,
                 ~func[2] & ~func[1] & ~func[0];          //100000
    wire i_sub = r_type & func[5] & ~func[4] & ~func[3] &
                 ~func[2] &  func[1] & ~func[0];          //100010
-      
-   //  please complete the deleted code.
-   
-   wire i_and =  
-   wire i_or  = 
 
-   wire i_xor = 
-   wire i_sll = 
-   wire i_srl = 
-   wire i_sra = 
-   wire i_jr  = 
+   wire i_and = r_type & func[5] & ~func[4] & ~func[3] & func[2] & ~func[1] & ~func[0];
+   wire i_or  = r_type & func[5] & ~func[4] & ~func[3] & func[2] & ~func[1] & func[0];
+
+   wire i_xor = r_type & func[5] & ~func[4] & ~func[3] & func[2] & func[1] & ~func[0];
+   wire i_sll = r_type & ~|func;
+   wire i_srl = r_type & ~func[5] & ~func[4] & ~func[3] & ~func[2] & func[1] & ~func[0];
+   wire i_sra = r_type & ~func[5] & ~func[4] & ~func[3] & ~func[2] & func[1] & func[0];
+   wire i_jr  = r_type & ~func[5] & ~func[4] & func[3] & ~func[2] & ~func[1] & ~func[0];
                 
    wire i_addi = ~op[5] & ~op[4] &  op[3] & ~op[2] & ~op[1] & ~op[0]; //001000
    wire i_andi = ~op[5] & ~op[4] &  op[3] &  op[2] & ~op[1] & ~op[0]; //001100
    
-   wire i_ori  =         // complete by yourself.
-   wire i_xori =   
-   wire i_lw   =   
-   wire i_sw   = 
-   wire i_beq  = 
-   wire i_bne  = 
-   wire i_lui  = 
-   wire i_j    = 
-   wire i_jal  = 
-   
-  
+   wire i_ori  = ~op[5] & ~op[4] & op[3] & op[2] & ~op[1] & op[0];
+   wire i_xori = ~op[5] & ~op[4] & op[3] & op[2] & op[1] & ~op[0];
+   wire i_lw   = op[5] & ~op[4] & ~op[3] & ~op[2] & op[1] & op[0];
+   wire i_sw   = op[5] & ~op[4] & op[3] & ~op[2] & op[1] & op[0];
+   wire i_beq  = ~op[5] & ~op[4] & ~op[3] & op[2] & ~op[1] & ~op[0];
+   wire i_bne  = ~op[5] & ~op[4] & ~op[3] & op[2] & ~op[1] & op[0];
+   wire i_lui  = ~op[5] & ~op[4] & op[3] & op[2] & op[1] & op[0];
+   wire i_j    = ~op[5] & ~op[4] & ~op[3] & ~op[2] & op[1] & ~op[0];
+   wire i_jal  = ~op[5] & ~op[4] & ~op[3] & ~op[2] & op[1] & op[0];
+
    assign pcsource[1] = i_jr | i_j | i_jal;
    assign pcsource[0] = ( i_beq & z ) | (i_bne & ~z) | i_j | i_jal ;
    
    assign wreg = i_add | i_sub | i_and | i_or   | i_xor  |
                  i_sll | i_srl | i_sra | i_addi | i_andi |
                  i_ori | i_xori | i_lw | i_lui  | i_jal;
-   
-   assign aluc[3] =      // complete by yourself.
-   assign aluc[2] = 
-   assign aluc[1] = 
-   assign aluc[0] = 
+
+   assign aluc[3] = i_sra;
+   assign aluc[2] = i_sub | i_or | i_lui | i_srl | i_sra;
+   assign aluc[1] = i_xor | i_lui | shift;
+   assign aluc[0] = i_and | i_or | shift;
    assign shift   = i_sll | i_srl | i_sra ;
 
-   assign aluimm  =      // complete by yourself.
-   assign sext    = 
-   assign wmem    = 
-   assign m2reg   = 
-   assign regrt   = 
-   assign jal     = 
-
+   assign aluimm  = r_type;
+   assign sext    = ~(i_andi | i_ori | i_xori);
+   assign wmem    = i_lw;
+   assign m2reg   = i_sw;
+   assign regrt   = ~r_type;
+   assign jal     = i_jal;
 endmodule
