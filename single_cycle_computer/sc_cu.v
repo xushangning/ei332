0629209f941339b9b3ed34c188b15eab18d9aaf8
Shining Xu <xushangning@sjtu.edu.cn>
2019-06-06 16:09:17 +0800

    Fix a bug in CU
    Fix wrong ALU operation when the instruction is beq or bne

--- single_cycle_computer/sc_cu.v
+++ single_cycle_computer/sc_cu.v
@@ -51,7 +51,7 @@ module sc_cu (op, func, z, wmem, wreg, regrt, m2reg, aluc, shift,
                  i_ori | i_xori | i_lw | i_lui  | i_jal;
 
    assign aluc[3] = i_sra;
-   assign aluc[2] = i_sub | i_or | i_ori | i_lui | i_srl | i_sra;
+   assign aluc[2] = i_sub | i_or | i_ori | i_lui | i_srl | i_sra | i_beq | i_bne;
    assign aluc[1] = i_xor | i_xori | i_lui | shift;
    assign aluc[0] = i_and | i_andi | i_or | i_ori | shift;
    assign shift   = i_sll | i_srl | i_sra ;
